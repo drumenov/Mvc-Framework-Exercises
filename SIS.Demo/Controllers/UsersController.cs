13ea7187950896307ea4adbfaeaab3c99930cff0
drumenov <drumenov@protonmail.com>
2018-10-21 14:57:22 +0300

    Edit of the UsersController
    Only one Return statement used in the methods for clarity;
    Added a new Method that registers a new user and sends a View that displays some of the user's credentials.

--- SIS.Demo/Controllers/UsersController.cs
+++ SIS.Demo/Controllers/UsersController.cs
@@ -2,10 +2,13 @@
 using SIS.Framework.Attributes;
 using SIS.Framework.Controllers.Base;
 using SIS.Demo.Services.Contracts;
+using SIS.Demo.ViewModels;
 
 namespace SIS.Demo.Controllers
 {
     public class UsersController : Controller {
+        
+
         private IUsersService usersService;
 
         public UsersController(IUsersService usersService) {
@@ -16,10 +19,32 @@ namespace SIS.Demo.Controllers
 
         [HttpPost]
         public IActionResult Login(LoginViewModel model) {
+
+            IActionResult result = this.RedirectToAction("/users/login");
+
             if (!ModelState.IsValid.HasValue || !ModelState.IsValid.Value) {
-                this.RedirectToAction("/users/login");
+                result = this.RedirectToAction("/users/login");
             }
-            return this.RedirectToAction("/");
+            if(usersService.ExistsByUsernameAndPassword(model.Username, model.Password)) {
+                result = this.RedirectToAction("/");
+            }
+            return result;
+        }
+
+        public IActionResult Register() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model) {
+            IActionResult result = this.RedirectToAction("/users/register/");
+            if(this.ModelState.IsValid.HasValue && this.ModelState.IsValid.Value) {
+                if (this.usersService.TryRegisterUser(model)) {
+                    this.ViewModel[nameof(model.Email).ToLower()] = model.Email;
+                    result = this.View("IndexLoggedIn");
+                }
+            }
+            return result;
         }
     }    
 }
