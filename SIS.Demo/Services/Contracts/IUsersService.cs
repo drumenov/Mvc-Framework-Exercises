414a165ed0c843a215cc23dbdc39318281ad02f1
drumenov <drumenov@protonmail.com>
2018-10-21 14:53:45 +0300

    UserServices Update
    1. Update the search criteria an existing user in the database - from Username to Email;
    2. A new method for adding a user introduced in the interface., an imlementation introduced into the class.

--- SIS.Demo/Services/Contracts/IUsersService.cs
+++ SIS.Demo/Services/Contracts/IUsersService.cs
@@ -1,7 +1,11 @@
-﻿namespace SIS.Demo.Services.Contracts
+﻿using SIS.Demo.ViewModels;
+
+namespace SIS.Demo.Services.Contracts
 {
     public interface IUsersService
     {
         bool ExistsByUsernameAndPassword(string username, string password);
+
+        bool TryRegisterUser(RegisterViewModel model);
     }
 }
--- SIS.Demo/Services/UsersService.cs
+++ SIS.Demo/Services/UsersService.cs
@@ -1,5 +1,7 @@
-﻿using IRunesWebApp.Data;
+﻿using SIS.Demo.Data;
+using SIS.Demo.Models;
 using SIS.Demo.Services.Contracts;
+using SIS.Demo.ViewModels;
 using SIS.Framework.Services;
 using System.Linq;
 
@@ -14,7 +16,29 @@ namespace SIS.Demo.Services
         }
 
         public bool ExistsByUsernameAndPassword(string username, string password) {
-            return this.dbContext.Users.Any(u => u.Username == username && u.Password == password.Hash());
+            return this.dbContext.Users.Any(u => u.Username == username && u.HashedPassword == password.Hash());
+        }
+
+        public bool TryRegisterUser(RegisterViewModel model) {
+            bool result = false;
+            if(this.dbContext.Users.Any(u => u.Username == model.Email)) {
+                return result;
+            }
+            else {
+                User newUser = new User {
+                    Username = model.Email,
+                    Email = model.Email,
+                    HashedPassword = model.Password
+                };
+                try {
+                    this.dbContext.Users.Add(newUser);
+                    this.dbContext.SaveChanges();
+                    return true;
+                }
+                catch {
+                    return false;
+                }
+            }
         }
     }
 }
