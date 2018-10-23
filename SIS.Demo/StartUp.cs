a869c5493ec0bb71ddffecdb2447a0175dd13695
drumenov <drumenov@protonmail.com>
2018-10-23 23:15:49 +0300

    Dependency Injection of Services
    Added needed services for running the Home, Login and Register pages.

--- SIS.Demo/StartUp.cs
+++ SIS.Demo/StartUp.cs
@@ -1,5 +1,9 @@
-﻿using SIS.Framework.Api.Contracts;
+﻿using SIS.Demo.Services;
+using SIS.Demo.Services.Contracts;
+using SIS.Framework.Api.Contracts;
+using SIS.Framework.Routers;
 using SIS.Framework.Services.Contracts;
+using SIS.WebServer.Api.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,7 +17,8 @@ namespace SIS.Demo
         }
 
         public void ConfigureServices(IDependencyContainer dependencyContainer) {
-            
+            dependencyContainer.RegisterDependency<IResourceRouter, ResourceRouter>();
+            dependencyContainer.RegisterDependency<IUsersService, UsersService>();
         }
     }
 }
