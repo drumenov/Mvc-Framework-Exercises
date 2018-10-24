96a29ef405ebf80496d5a108c893b4b221bbe307
drumenov <drumenov@protonmail.com>
2018-10-25 00:18:05 +0300

    Album Service Interface was added
    Albums Service Interface and its implimentation was added.

---
+++ SIS.Demo/Services/AlbumsService.cs
@@ -0,0 +1,26 @@
+﻿using SIS.Demo.Data;
+using SIS.Demo.Models;
+using SIS.Demo.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIS.Demo.Services
+{
+    public class AlbumsService : IAlbumsService
+    {
+        private readonly IRunesDbContext dbContext;
+
+        public AlbumsService(IRunesDbContext dbContext) {
+            this.dbContext = dbContext;
+        }
+
+        public ICollection<Album> GetAllAlbums(string username) {
+            var tre = this.dbContext.Albums.ToArray();
+            var ert = this.dbContext.UsersAlbums.ToArray();
+            ICollection<Album> allUserAlbums = this.dbContext.Users.First(u => u.Username == username).Albums.Select(ua => ua.Album).ToList();
+            return allUserAlbums;
+        }
+    }
+}
---
+++ SIS.Demo/Services/Contracts/IAlbumsService.cs
@@ -0,0 +1,12 @@
+﻿using SIS.Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Demo.Services.Contracts
+{
+    public interface IAlbumsService
+    {
+        ICollection<Album> GetAllAlbums(string userId);
+    }
+}
