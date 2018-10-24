eef82decfadb92f78dbe5550839b015421bfdf0c
drumenov <drumenov@protonmail.com>
2018-10-25 00:19:56 +0300

    ViewModel that displays album collection
    A ViewModel that displays album collection was added. Its child views were also implemented.

---
+++ SIS.Demo/ViewModels/AlbumsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SIS.Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Demo.ViewModels
+{
+    public class AlbumsViewModel
+    {
+        public ICollection<Album> Albums { get; set; }
+    }
+}
