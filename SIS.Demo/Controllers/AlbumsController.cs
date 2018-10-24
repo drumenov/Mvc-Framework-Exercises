31f5628d2d108dae4a6e02b55ec5aba6a51745de
drumenov <drumenov@protonmail.com>
2018-10-25 00:14:01 +0300

    HomeController Edit
    Updated the HomeController to return the home view of the logged in User.

---
+++ SIS.Demo/Controllers/AlbumsController.cs
@@ -0,0 +1,33 @@
+﻿using SIS.Demo.Models;
+using SIS.Demo.Services.Contracts;
+using SIS.Demo.ViewModels;
+using SIS.Framework.ActionResults.Contracts;
+using SIS.Framework.Attributes;
+using SIS.Framework.Attributes.Action;
+using SIS.Framework.Controllers.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Demo.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private readonly IAlbumsService albumsService;
+
+        public AlbumsController(IAlbumsService albumsService) {
+            this.albumsService = albumsService;
+        }
+
+        [HttpGet]
+        [Authorise]
+        public IActionResult All() {
+            ICollection<Album> albums = this.albumsService.GetAllAlbums(this.Identity.Username);
+            AlbumsViewModel avm = new AlbumsViewModel {
+                Albums = albums
+            };
+            this.ViewModel.Data["AlbumsViewModel"] = avm;
+            return this.View("all-user-albums");
+        }
+    }
+}
