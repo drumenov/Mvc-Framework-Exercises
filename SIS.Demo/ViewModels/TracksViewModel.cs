2651db2abf6372c9aa8e3ef11f3ebef29c3ebd5f
drumenov <drumenov@protonmail.com>
2018-10-25 23:31:41 +0300

    ViewModel
    A TracksViewModel was added to help render data to the user.

---
+++ SIS.Demo/ViewModels/TracksViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Demo.ViewModels
+{
+    public class TracksViewModel
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string TrackSource { get; set; }
+
+        public string AlbumId { get; set; }
+    }
+}
