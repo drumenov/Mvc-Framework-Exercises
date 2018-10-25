a56d9268f4c60c0fde56ebeb6316a7961f39003d
drumenov <drumenov@protonmail.com>
2018-10-25 23:32:25 +0300

    TrackController Introduced
    A TracksController was introduced that manages Tracks information.

---
+++ SIS.Demo/Controllers/TracksController.cs
@@ -0,0 +1,36 @@
+﻿using SIS.Demo.Models;
+using SIS.Demo.Services.Contracts;
+using SIS.Demo.ViewModels;
+using SIS.Framework.ActionResults.Contracts;
+using SIS.Framework.Controllers.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Demo.Controllers
+{
+    public class TracksController : Controller
+    {
+        private readonly ITrackService trackService;
+
+        public TracksController(ITrackService trackService) {
+            this.trackService = trackService;
+        }
+
+        public IActionResult Details() {
+
+            Track track = this.trackService.GetTrackById(this.Request.QueryData["trackId"].ToString());
+            if(track == null) {
+                this.ViewModel.Data["Error"] = "No such track."; //TODO: Check what this does.
+            }
+            TracksViewModel tracksViewModel = new TracksViewModel {
+                Name = track.Name,
+                Price = track.Price,
+                TrackSource = track.Link,
+                AlbumId = this.Request.QueryData["albumId"].ToString()
+            };
+            this.ViewModel.Data["TracksViewModel"] = tracksViewModel;
+            return this.View();
+        }
+    }
+}
