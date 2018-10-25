0bb5b7d102c43ada16ffa5146553cbdd9df95a56
drumenov <drumenov@protonmail.com>
2018-10-25 20:25:01 +0300

    Update SIS.Demo/ViewModels/AlbumDetailsViewModel.cs
    Added a view model that displays the details about a single album.

---
+++ SIS.Demo/ViewModels/AlbumDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SIS.Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Demo.ViewModels
+{
+    public class AlbumDetailsViewModel
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageSource { get; set; }
+
+        public IEnumerable<Track> Tracks { get; set; }
+    }
+}
