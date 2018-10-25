1c910eb399e2a6661c4664600ea9fa4eeff26e35
drumenov <drumenov@protonmail.com>
2018-10-25 23:25:37 +0300

    Introduced the TrackViewModel
    This class is used to pass around the Id of the track as well as the Album's ID. The ViewModel is Used to go to a selected track form a given album and afterwards to be able to come back to the same album.

---
+++ SIS.Demo/ViewModels/TrackViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Demo.ViewModels
+{
+    public class TrackViewModel
+    {
+        public string Id { get; set; }
+
+        public string AlbumId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
