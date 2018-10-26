da1ae550cf18e8eebf71362e86c980b14fcbfe74
drumenov <drumenov@protonmail.com>
2018-10-26 19:06:58 +0300

    AlbumInputModel
    A model that is used to receive the data needed for registering an album.

---
+++ SIS.Demo/ViewModels/InputAlbumModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Demo.ViewModels
+{
+    public class InputAlbumModel
+    {
+        public string Name { get; set; }
+
+        public string Cover { get; set; }
+    }
+}
