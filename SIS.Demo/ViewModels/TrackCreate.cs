b8605a1226cc2d609563f2c73de689ca49f1eb52
drumenov <drumenov@protonmail.com>
2018-10-26 19:07:49 +0300

    TrackCreate
    A model that receives the data needed to create a track.

---
+++ SIS.Demo/ViewModels/TrackCreate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Demo.ViewModels
+{
+    public class TrackCreate
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Link { get; set; }
+    }
+}
